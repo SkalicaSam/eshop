db4924442efbd7299654840d128ed263c4cf32d0
SkalicaSam <samcostehlik@gmail.com>
2018-09-12 15:47:24 +0200

    Created view for event details, added links to event listing view

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('events/', views.event_listing, name='events')
+    path('events/', views.event_listing, name='events'),
+    path('event/<str:name>', views.event_detail, name='detail'),
 ]
